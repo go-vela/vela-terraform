63312901037504f42a88a638abe83cfe2bb1abc5
Neal <neal.coleman@target.com>
2020-04-30 15:20:00 -0500

    chore: add skeleton for plugin and config (#3)
    * chore: add skeleton terraform commands
    
    * chore: add skeleton for plugin and config
    
    * fix: update typo

---
+++ cmd/vela-terraform/config_test.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package main
+
+import (
+	"testing"
+)
+
+func TestTerraform_Config_New(t *testing.T) {
+
+}
+
+func TestTerraform_Config_Validate(t *testing.T) {
+
+}
