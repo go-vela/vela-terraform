64a59d32cde12e1e5a3c86a048c98386bc4d8b88
Jordan <Jordan.Brockopp@target.com>
2021-03-01 09:35:33 -0600

    chore: address linter feedback

--- cmd/vela-terraform/main.go
+++ cmd/vela-terraform/main.go
@@ -263,6 +263,8 @@ func main() {
 }
 
 // run executes the plugin based off the configuration provided.
+//
+// nolint: funlen // ignore function length due to comments and flags
 func run(c *cli.Context) error {
 	// set the log level for the plugin
 	switch c.String("log.level") {
