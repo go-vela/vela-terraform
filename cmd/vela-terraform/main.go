02cb207a069a770009603118af755cdb481f484b
Kayla McKay <kaymckay@umich.edu>
2022-03-03 14:11:01 -0600

    remove lll false pos

--- cmd/vela-terraform/main.go
+++ cmd/vela-terraform/main.go
@@ -228,7 +228,6 @@ func main() {
 			Name:     "plan.destroy",
 			Usage:    "destroy all resources managed by the given configuration and state",
 		},
-		// nolint: lll // skip line length due to long parameter name
 		&cli.BoolFlag{
 			EnvVars:  []string{"PARAMETER_DETAILED_EXIT_CODE", "TERRAFORM_DETAILED_EXIT_CODE"},
 			FilePath: "/vela/parameters/terraform/detailed_exit_code,/vela/secrets/terraform/detailed_exit_code",
